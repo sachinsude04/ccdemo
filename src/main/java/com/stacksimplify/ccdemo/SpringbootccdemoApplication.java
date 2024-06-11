34e8d14cbbc8c699c198fca8c432ef412c930ec7
sachinsude04 <sachinsude040695@gmail.com>
2024-06-11 12:20:57 +0530

    first push

---
+++ src/main/java/com/stacksimplify/ccdemo/SpringbootccdemoApplication.java
@@ -0,0 +1,15 @@
+package com.stacksimplify.ccdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootccdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootccdemoApplication.class, args);
+		
+		System.out.println("Hello And welcome to AWS cloud CICD deployment, this is much interesting course");
+	}
+
+}
